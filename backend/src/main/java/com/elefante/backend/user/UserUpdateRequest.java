e7ea677ca9afcc5431c827f923f903adc22acf59
yamacinelli <yagom.ym@gmail.com>
2026-08-23 23:57:37 -0300

    fix(user): adjust user update flow

--- backend/src/main/java/com/elefante/backend/user/UserUpdateRequest.java
+++ backend/src/main/java/com/elefante/backend/user/UserUpdateRequest.java
@@ -2,11 +2,16 @@ package com.elefante.backend.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
     @NotNull Integer id,
     @Email String email,
-    @Size(min = 8) String password,
+    @Pattern(
+        regexp = "^(?=(?:.*[!@#$%^&*()\\-_=+\\[\\]{};:'\",.<>/?\\\\|`~]){2,})(?=(?:.*\\d){2,}).{8,}$",
+        message = "Password must be at least 8 characters, with at least 2 digits and 2 special characters"
+    )
+    String password,
     @Size(min = 4) String role
 ) { }
